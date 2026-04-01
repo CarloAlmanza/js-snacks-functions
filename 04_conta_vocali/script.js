9280522b911a90be964b103db2c14ddedba35601
CarloAlmanza <almanzacarlo6@gmail.com>
2026-04-01 17:57:31 +0200

    implementazione esercizio 04-conta-vocali

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,10 +4,18 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
-
+function vocali(parola) {
+    let contatore = 0;
+    for (let i = 0; i < parola.length; i++) {
+        if (parola[i] === 'a' || parola[i] === 'e' || parola[i] === 'i' || parola[i] === 'o' || parola[i] === 'u') {
+            contatore++;
+        }
+    }
+    return contatore;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(vocali(word));
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
