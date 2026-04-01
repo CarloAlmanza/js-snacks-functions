8bb68ae57d1d8226b25658263ae2034723238435
CarloAlmanza <almanzacarlo6@gmail.com>
2026-04-01 18:17:12 +0200

    implementazione esercizio 05-bonus

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -9,10 +9,22 @@ const name = 'Mario';
 
 
 // Dichiara la funzione qui.
-
+function saluto(nome) {
+    const data = new Date();
+    const ora = data.getHours();
+    let messaggio = '';
+    if (ora < 13) {
+        messaggio = `buongiorno ${nome}`;
+    } else if (ora >= 13 && ora < 17) {
+        messaggio = `buon pomeriggio ${nome}`;
+    } else {
+        messaggio = `buonasera ${nome}`;
+    }
+    return messaggio;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(saluto(name));
 
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
